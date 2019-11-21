84511cfff2fd3b543d54f6c716e752627fa8d1c4
gil serrano <gil.serrano.97@gmail.com>
2019-11-21 20:07:00 +0000

    Included Aruco Measurement class

--- ekf.py
+++ ekf.py
@@ -1,8 +1,10 @@
+#! /usr/bin/env python
 import numpy as np
 import sys
 
 import rospy
 from nav_msgs.msg import Odometry
+from aruco_pose_subscriber import *
 
 class MotionModel():
 
@@ -109,6 +111,22 @@ class Measurement():
         self.id = id
         self.r  = distance
         self.theta = angle
+
+
+class Aruco():
+    @staticmethod
+    def callback(data):
+        Aruco.list = []
+        for i in range(len(data)):
+            aruco_id = data.markers[i].id
+            r,theta  = convert_pose(data.markers[i].pose.pose.position.x, data.markers[i].pose.pose.position.y)
+            Aruco.list.append(Measurement(aruco_id, r, theta))
+
+    
+def convert_pose(pose_x, pose_y):
+    r = np.sqrt(pose_x**2 + pose_y**2)
+    angle = np.arctan2(pose_y, pose_x)
+    return [r, angle]
 
 class Odom():
 
@@ -252,15 +270,17 @@ def main():
 
     rospy.init_node('pose_listener', anonymous=True)
     rospy.Subscriber('/pioneer/pose', Odometry, Odom.retrieveOdom)
-
+    rospy.init_node('aruco_pose_listener', anonymous=True)
+    rospy.Subscriber("/aruco_marker_publisher/markers", MarkerArray, Aruco.callback)
+    
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
 
         ekf.prediction_step(Odom.v, Odom.w, Odom.dt)
         ekf.print_state()
 
-        measurs = [Measurement(1, 2, 0.4), Measurement(2, 2, 0.25)]
-        ekf.update_step(measurs)
+        #measurs = [Measurement(1, 2, 0.4), Measurement(2, 2, 0.25)]
+        ekf.update_step(Aruco.list)
         ekf.print_state()
 
         rate.sleep()
