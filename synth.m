d9405bc0c2d4994d3aa698452abfeb0af1a642ce
JaFoste99 <duartecorreiascs@hotmail.com>
2019-12-06 10:00:18 +0000

    fixed and pretty

--- synth.m
+++ synth.m
@@ -1,19 +1,21 @@
 clear classes
 
-pymod_matlab = py.importlib.import_module('synth_matlab');
-pymod_ekf    = py.importlib.import_module('ekf');
-pymod_models = py.importlib.import_module('synth_base');
+addpath('MATLAB_code')
 
-py.importlib.reload(pymod_matlab);
+pymod_ekf = py.importlib.import_module('ekf');
+pymod_models = py.importlib.import_module('synth_base');
+pymod_matlab = py.importlib.import_module('synth_matlab');
+
 py.importlib.reload(pymod_ekf);
 py.importlib.reload(pymod_models);
+py.importlib.reload(pymod_matlab);
 
 % Parâmetros do ekf
-Q_diag = [0.1, 0.1];
-sigma  = 0.02;
+Q_diag = [0.5, 0.5];
+sigma  = 0.04;
 
 % Variâncias dos sensores
-mu_odom = [0.1, 0.05];
+mu_odom = [0.8, 0.5];
 mu_obse = [0.2, 0.08];
 
 ekf = py.synth_matlab.Matlab_EKF(Q_diag, sigma, mu_odom, mu_obse);
@@ -53,7 +55,29 @@ estim_path_y = zeros(npoints, 1);
 noise_path_x = zeros(npoints, 1);
 noise_path_y = zeros(npoints, 1);
 
-figure('units','normalized','outerposition',[0 0 1 1])
+fig = figure('units','normalized','outerposition',[0 0 1 1]);
+set(fig,'defaultLegendAutoUpdate','off')
+hold on
+grid minor
+xlim(xsize)
+ylim(ysize)
+
+h = zeros(3, 1);
+h(1) = plot(NaN, NaN, 'g.-');
+h(2) = plot(NaN, NaN, 'b.-');
+h(3) = scatter(NaN, NaN, 'ob');
+h(4) = plot(NaN, NaN, 'r.-');
+h(5) = scatter(NaN, NaN, 'xg');
+h(6) = scatter(NaN, NaN, 'xr');
+h(7) = scatter(NaN, NaN, 'or');
+legend(h, 'Robot','Estimate', 'Estimate Covariance' ,'Odom', ...
+    'Landmark', 'Landmark Estimation', 'Landmark Covariance', ...
+    'Location', 'Northeast');
+
+title('EKF Simulation')
+xlabel('x [m]')
+ylabel('y [m]')
+
 for i = 1:npoints * skipped
     w = w + w_var;
     v = v + v_var;
@@ -64,17 +88,13 @@ for i = 1:npoints * skipped
     if mod(i, skipped) == 0
         k = i / skipped;
         
-        pause(0.001)
-        clf
+        pause(0.0001)
+        cla
 
         estimate   = np_matlab(ekf.get_estimate());
         covariance = np_matlab(ekf.get_covariance());
 
         scatter(landmarks_x, landmarks_y, 'gx')
-        hold on
-        grid minor
-        xlim(xsize)
-        ylim(ysize)
 
         current_robot = np_matlab(ekf.get_robot());
         current_noise = np_matlab(ekf.get_odom());
@@ -82,23 +102,19 @@ for i = 1:npoints * skipped
         robot_path_x(k) = current_robot(1);
         robot_path_y(k) = current_robot(2);
         plot(robot_path_x(1:k), robot_path_y(1:k), 'g.-')
-        hold on
         
         noise_path_x(k) = current_noise(1);
         noise_path_y(k) = current_noise(2);
         plot(noise_path_x(1:k), noise_path_y(1:k), 'r.-')
-        hold on
         
         estim_path_x(k) = estimate(1);
         estim_path_y(k) = estimate(2);
         plot(estim_path_x(1:k), estim_path_y(1:k), 'blue.-')
-        hold on
 
         try 
             h = error_ellipse(covariance(1:2,1:2), estimate(1:2));
             h.Color = 'Blue';
             plot(h)
-            hold on
         catch
         end
         
@@ -107,12 +123,10 @@ for i = 1:npoints * skipped
             x = 4 + j * 2;
             y = 5 + j * 2;
             scatter(estimate(x),estimate(y), 'rx')
-            hold on
             try 
                 h = error_ellipse(covariance(x:y,x:y), estimate(x:y));
                 h.Color = 'Red';
                 plot(h)
-                hold on
             catch
             end
         end
