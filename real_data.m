19ec40664d175669981575a0965d0a967858a92b
JaFoste99 <duartecorreiascs@hotmail.com>
2019-12-09 00:11:17 +0000

    add treated mat data ready to be analysed

--- real_data.m
+++ real_data.m
@@ -1,12 +1,13 @@
-bag = rosbag('duarte.bag');
-
-odom_bag  = select(bag, 'Topic', '/pioneer/pose');
-mocap_bag = select(bag, 'Topic', '/mocap_node/Robot_1/ground_pose');
-
-odom = timeseries(odom_bag, 'Pose.Pose.Position.X', 'Twist.Twist.Angular.Z');
-odom.Time = odom.Time - bag.StartTime;
-
-mocap = timeseries(mocap_bag, 'X', 'Y');
-mocap.Time = mocap.Time - bag.StartTime;
+% bag = rosbag('duarte.bag');
+% 
+% odom_bag  = select(bag, 'Topic', '/pioneer/pose');
+% mocap_bag = select(bag, 'Topic', '/mocap_node/Robot_1/ground_pose');
+% aruco_bag = select(bag, 'Topic', '/aruco_marker_publisher/markers');
+% 
+% odom = timeseries(odom_bag, 'Pose.Pose.Position.X', 'Twist.Twist.Angular.Z');
+% odom.Time = odom.Time - bag.StartTime;
+% 
+% mocap = timeseries(mocap_bag, 'X', 'Y');
+% mocap.Time = mocap.Time - bag.StartTime;
 
 
